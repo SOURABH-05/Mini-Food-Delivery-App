09a2df1b1e1125b559b2f56d723c4b58152ececd
SOURABH <s.dharra88@gmail.com>
2025-07-09 08:26:19 +0530

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,7 +1,7 @@
 export default function Banner() {
   return (
     <div className="bg-yellow-400 text-center p-6 rounded-xl text-xl font-semibold">
-      Get 20% off on your first order! 🍔🍕🥤
+      Get 20% off on your first order! 🍔🍕
     </div>
   );
 }
